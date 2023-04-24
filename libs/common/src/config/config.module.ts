403985bac8febcf328ff4eac37e8ef951a6789cf
TonyThorne <anthony.thorne@gmail.com>
2023-04-24 19:39:30 +0100

    Feat: Added Abstract database model

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import * as joi from 'joi';
 import {
   ConfigService,
   ConfigModule as NestConfigModule,
 } from '@nestjs/config';
 
 @Module({
-  imports: [NestConfigModule.forRoot()],
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: joi.object({
+        MONGODB_URI: joi.string().required(),
+      }),
+    }),
+  ],
   providers: [ConfigService],
   exports: [ConfigService],
 })
+// eslint-disable-next-line prettier/prettier
 export class ConfigModule { }
