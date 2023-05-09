f058b90347e5589c83f78c611620119e5d9c21be
TonyThorne <anthony.thorne@gmail.com>
2023-05-09 22:03:11 +0100

    chore: try to remove slass-calidator error

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -2,16 +2,19 @@ import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateReservationDto {
-  @IsDate()
   // class-transformer is used to convert the JSON string to a Date object
+  @IsDate()
   @Type(() => Date)
   startDate: Date;
+
   @IsDate()
   @Type(() => Date)
   endDate: Date;
+
   @IsString()
   @IsNotEmpty()
   placeId: string;
+
   @IsString()
   @IsNotEmpty()
   invoiceId: string;
